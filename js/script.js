5d6b49ccb71e1cccaf3f7082d6e0a309a31fe414
Dima Laptop <dinerdima1307@gmail.com>
2023-02-26 22:17:46 +0600

    практика ч.1 выполнено

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 'use strict';
 /* eslint-disable no-unused-vars */
 
-console.log('Hello world');
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+let movies1 = prompt('Один из последних просмотренных фильмов?');
+let rating1 = prompt('Насколько вы его оцениет?');
+personalMovieDB.movies[movies1] = rating1;
+
+let movies2 = prompt('Один из последних просмотренных фильмов?');
+let rating2 = prompt('Насколько вы его оцениет?');
+personalMovieDB.movies[movies2]= rating2;
+ 
+
+console.log(personalMovieDB.movies);
+
