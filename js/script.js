36ce73f918c57fc55a5141cc321206d02a98917e
Dima Laptop <dinerdima1307@gmail.com>
2023-02-27 00:14:47 +0600

    упражение по циклам 3 выполнено

--- js/script.js
+++ js/script.js
@@ -1,25 +1,75 @@
-'use strict';
-/* eslint-disable no-unused-vars */
+// Место для первой задачи
+function firstTask() {
+	// Пишем решение вот тут
+	for (let i = 5; i < 11; i++){
+		console.log(i);
+	}
+    
+}
 
+// Место для второй задачи
+function secondTask() {
+	// Пишем решение вот тут
+	for (let j = 20; j>9; j--){
+		if(j===13){
+			break;
+		}
+		console.log(j);
+	}
+    
+}
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+// Место для третьей задачи
+function thirdTask() {
+	// Пишем решение вот тут
+	function thirdTask() {
+	// Пишем решение вот тут
+		for (let k = 2; k < 11; k++){
+			if (k % 2 === 0 ){
+				console.log(k);
+			}
+		} 
+    
+	}
+	thirdTask();
+}
 
-let personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// Место для четвертой задачи
 
-let movies1 = prompt('Один из последних просмотренных фильмов?');
-let rating1 = prompt('Насколько вы его оцениет?');
-personalMovieDB.movies[movies1] = rating1;
+// Цикл, который нужно переписать:
 
-let movies2 = prompt('Один из последних просмотренных фильмов?');
-let rating2 = prompt('Насколько вы его оцениет?');
-personalMovieDB.movies[movies2]= rating2;
- 
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
 
-console.log(personalMovieDB.movies);
+function fourthTask() {
+	// Пишем решение вот тут
+	let x = 2;
+	while (x <= 15){
+		x++;
+		if (x%2 !==0){
+			console.log(x);
+		}
+	}
+    
+}
 
+// Место для пятой задачи
+
+function fifthTask() {
+	const arrayOfNumbers = [];
+
+	// Пишем решение вот тут
+	let x = 0;
+	for (let i = 5; i < 11; i++){
+		arrayOfNumbers[x]=i;
+		x++;
+	}
+    
+	// Не трогаем
+	return arrayOfNumbers;
+}
