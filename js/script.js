5caace692b44a37c07fedca7a7f060fbe2a6ab78
Dima PC <dinerdima1307@gmail.com>
2023-02-27 15:10:28 +0600

    коммит с рабочего пк

--- js/script.js
+++ js/script.js
@@ -1,75 +1,11 @@
-// Место для первой задачи
-function firstTask() {
-	// Пишем решение вот тут
-	for (let i = 5; i < 11; i++){
-		console.log(i);
-	}
-    
-}
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
 
-// Место для второй задачи
-function secondTask() {
-	// Пишем решение вот тут
-	for (let j = 20; j>9; j--){
-		if(j===13){
-			break;
-		}
-		console.log(j);
-	}
-    
-}
 
-// Место для третьей задачи
-function thirdTask() {
-	// Пишем решение вот тут
-	function thirdTask() {
-	// Пишем решение вот тут
-		for (let k = 2; k < 11; k++){
-			if (k % 2 === 0 ){
-				console.log(k);
-			}
-		} 
-    
-	}
-	thirdTask();
-}
-
-// Место для четвертой задачи
-
-// Цикл, который нужно переписать:
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
+// let x = data.length
+// for (let i= 0 ; i < data.length; i++){
+// 	result[i] = data[x];
+// 	x--;
 // }
 
-function fourthTask() {
-	// Пишем решение вот тут
-	let x = 2;
-	while (x <= 15){
-		x++;
-		if (x%2 !==0){
-			console.log(x);
-		}
-	}
-    
-}
-
-// Место для пятой задачи
-
-function fifthTask() {
-	const arrayOfNumbers = [];
-
-	// Пишем решение вот тут
-	let x = 0;
-	for (let i = 5; i < 11; i++){
-		arrayOfNumbers[x]=i;
-		x++;
-	}
-    
-	// Не трогаем
-	return arrayOfNumbers;
-}
+console.log("sdfsddsfsdsadfsadfgitfsdfsdfsdfsdfsd");
