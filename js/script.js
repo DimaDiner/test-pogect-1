509bc116003b9c6bc7e17eba457a2a8fecadf148
Dima Laptop <dinerdima1307@gmail.com>
2023-03-04 22:56:29 +0600

    Упражнение по написанию кода 6. Выполнено

--- js/script.js
+++ js/script.js
@@ -1,51 +1,31 @@
-/* Задание на урок:
+// Место для первой задачи
+function sayHello(name) {
+    return `Привет, ${name}!`
+}
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-'use strict';
-
-// Код возьмите из предыдущего домашнего задания
-
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-
-let personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
+// Место для второй задачи
+function returnNeighboringNumbers(a) {
+    let arr = [];
+    arr[0] = a - 1;
+    arr[1] = a;
+    arr[2] = a + 1;
+    return arr;
 };
 
-start: for (let i = 0; i < 2; i++){
-    let movies1 = prompt('Один из последних просмотренных фильмов?');
-    let rating1 = prompt('Насколько вы его оцениет?');
+// Место для третьей задачи
+function getMathResult(x,y) {
+    if (typeof(y) != Number && y != 0 && y > 0){
+        let answer = x
+        let promPeremenaia = x
+        for (let i = 1; i < y; i++){
+            answer += '---'
+            answer += promPeremenaia + x
+            promPeremenaia = promPeremenaia + x
+        }
+        return answer
 
-    if (movies1 != null && rating1 != null && movies1 != '' && rating1 != '' && movies1.length < 50 && rating1.length < 50){
-       personalMovieDB.movies[movies1] = rating1
-    } else {
-        i--;
-    }
-
+        } else {
+            return x
+        }
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов')
-} else if ( personalMovieDB.count >= 10 && personalMovieDB.count <=20){
-    alert('Вы классический зритель')
-} else if (personalMovieDB.count > 20){
-    alert('Вы киноман')
-} else {    
-    alert('Произошла ошибка')
-}
-
-console.log(personalMovieDB.movies);
