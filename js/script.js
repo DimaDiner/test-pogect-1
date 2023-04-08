5fa43b070c3a3d0772881054f6a4f381cacaca36
Dima Laptop <dinerdima1307@gmail.com>
2023-04-08 20:01:48 +0600

    упражнение по написанию кода 13 выполенно

--- js/script.js
+++ js/script.js
@@ -1,87 +1,55 @@
-'use strict';
-
-
-let personalMovieDB = {
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-
-    start: function() {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
         }
-    },
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+}
 
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            let movies1 = prompt('Один из последних просмотренных фильмов?');
-            let rating1 = prompt('Насколько вы его оцениет?');
+function isBudgetEnough(data) {
+
+
+       let totalArea = 0;
+       
+    //    Считаем площадь одного магазина и прибавляем в общую площадь
+       data.shops.forEach(function(item, i){
+        let dataShop = [];
+        let areaShop = 0;
+        for (let key in item){
+            dataShop.push(item[key])
+        }
+        areaShop = dataShop[0] * dataShop[1];
+        totalArea += areaShop
+       });
+
+    //    Расчитываем объем торгового центра
+        let totalVolume = totalArea*data.height;
     
-            if (movies1 != null && rating1 != null && movies1 != '' && rating1 != '' && movies1.length < 50 && rating1.length < 50){
-               personalMovieDB.movies[movies1] = rating1
-            } else {
-                i--; 
-            }
-        }
-    },
-
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            alert('Просмотрено довольно мало фильмов')
-        } else if ( personalMovieDB.count >= 10 && personalMovieDB.count <=20){
-            alert('Вы классический зритель')
-        } else if (personalMovieDB.count > 20){
-            alert('Вы киноман')
-        } else {    
-            alert('Произошла ошибка')
-        }
-    },
-
-    showMyBD: function() {
-        if (personalMovieDB.privat === false) {
-            console.log(personalMovieDB);
-        }
-    },
-
-    writeYourGenres: function() {
-        for (let i = 1; i < 4; i++){
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`)
-            if (genre === '' || genre === null) {
-                i--;
-            } else {
-                personalMovieDB.genres[i-1] = genre
-            }
-        }
-
-        personalMovieDB.genres.forEach(function(item, i){
-            console.log(`Любимый жанр #${i+1} - это ${item}`)
-        })
-    },
-
-    toggleVisibleMyDB: function(){
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false
+    //    Определяем хватит ли бюджета
+        if (data.budget >= totalVolume*30){
+            console.log('Бюджета достаточно');
+            return 'Бюджета достаточно'
         } else {
-            personalMovieDB.privat = true
+            console.log('Бюджета недостаточно');
+            return 'Бюджета недостаточно'
         }
-    }
 
 };
 
-
-// personalMovieDB.start();
-
-// personalMovieDB.rememberMyFilms();
-
-// personalMovieDB.detectPersonalLevel();
-
-personalMovieDB.writeYourGenres();
-
-// personalMovieDB.toggleVisibleMyDB();
-
-// personalMovieDB.showMyBD();
-
-console.log(personalMovieDB)
+isBudgetEnough(shoppingMallData)
